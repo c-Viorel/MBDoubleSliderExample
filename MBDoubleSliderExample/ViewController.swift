4e078362a2fc5744a70db1b90f8a85c438d4e317
viorelPorumbescu <viorelporumbescu@outlook.com>
2017-01-19 21:35:14 +0200

    Initial Commit

---
+++ MBDoubleSliderExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MBDoubleSliderExample
+//
+//  Created by Viorel Porumbescu on 19/01/2017.
+//  Copyright © 2017 Viorel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
